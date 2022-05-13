793504df5359d3ef015eec1f656180fd03ec10bc
Komal <divyavairagade@gmail.com>
2022-05-13 12:08:41 +0530

    firstname is added

---
+++ RFP_RegexAss/Pattern.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RFP_RegexAss
+{
+    internal class Pattern
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z][a-z]{3,}$";
+        public bool validateFirstName(string firstname)
+        {
+            return Regex.IsMatch(firstname, REGEX_FIRSTNAME);
+        }
+    }
+}
--- RFP_RegexAss/Program.cs
+++ RFP_RegexAss/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using RFP_RegexAss;
+
 Console.WriteLine("User Registration");
+Console.WriteLine( );
+
+Pattern patterns = new Pattern();
+Console.WriteLine(patterns.validateFirstName("Komal"));
