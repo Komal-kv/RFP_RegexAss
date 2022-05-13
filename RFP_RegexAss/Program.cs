770c8b1ff8c1c9f7f8deb2c12489679a221c2eac
Komal <divyavairagade@gmail.com>
2022-05-13 14:10:45 +0530

    Passed the all samples of email

--- RFP_RegexAss/Program.cs
+++ RFP_RegexAss/Program.cs
@@ -7,6 +7,17 @@ Console.WriteLine( );
 Pattern patterns = new Pattern();
 Console.WriteLine(patterns.validateFirstName("Komal"));
 Console.WriteLine(patterns.validateLastName("Vairagade"));
-Console.WriteLine(patterns.validateEmail("divyavairagade@gmail.com"));
+Console.WriteLine( );
+
+Console.WriteLine(patterns.validateEmail("abc@yahoo.com"));
+Console.WriteLine(patterns.validateEmail("abc-100@yahoo.com"));
+Console.WriteLine(patterns.validateEmail("abc.100@yahoo.com"));
+Console.WriteLine(patterns.validateEmail("abc111@abc.com"));
+Console.WriteLine(patterns.validateEmail("abc-100@abc.net"));
+Console.WriteLine(patterns.validateEmail("abc.100@abc.com.au"));
+Console.WriteLine(patterns.validateEmail("abc@gmail.com.com"));
+Console.WriteLine(patterns.validateEmail("abc+100@yahoo.com"));
+Console.WriteLine( );
+
 Console.WriteLine(patterns.validatePhonNo("91 8788254798"));
 Console.WriteLine(patterns.validatePassword("Bridge001labz@"));
